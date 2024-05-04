2acebfb1d36ac3c3fc7f0daa7a8299e0c818d6a7
Tatyene <tatyene0409@yahoo.com.br>
2024-05-04 19:13:02 -0300

    feat: criação do header, footer e home - falta ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', title: 'Home', loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent)}
+];
